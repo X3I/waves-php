45cafb18855dc5116b5950f82ecfa3435734a0d7
uwu <hello@shadyx.com>
2018-01-05 13:53:17 +0000

    Update waves.class.php
    Added new functions and docblocks that are not very useful

--- waves.class.php
+++ waves.class.php
@@ -4,11 +4,17 @@ Class Waves {
     
     private $node;
     
-    const FIRST_ASSET_BLOCK = '236967';
-    
     public function __construct($node) {
-        $this->node = $node;
+        $this->node       = $node;
+        $this->firstAsset = '236967'; // The first block that contained an Asset
     }
+    
+    /**
+     * Send request using curl
+     * @param  string $endpoint 
+     * @param  string $type     
+     * @return string
+     */
     
     public function request($endpoint, $type = '') {
         $curl = curl_init();
@@ -21,43 +27,139 @@ Class Waves {
         curl_close($curl);
     }
     
+    /**
+     * Get blockchain height
+     * @return string
+     */
+    
     public function blockHeight() {
         $data = $this->request('/blocks/height');
         return $data;
     }
+    
+    /**
+     * Check whether address is valid or not
+     * @param  string $args 
+     * @return string
+     */
     
     public function validateAddress($args) {
         $data = $this->request('/addresses/validate/' . $args);
         return $data;
     }
     
+    /**
+     * Account's balance
+     * @param  string $args 
+     * @return string
+     */
+    
     public function addressBalance($args) {
         $data = $this->request('/addresses/balance/' . $args);
         return $data;
     }
+    
+    /**
+     * Account's balances for all assets
+     * @param  string $args 
+     * @return string
+     */
     
     public function assetBalance($args) {
         $data = $this->request('/assets/balance/' . $args);
         return $data;
     }
     
+    /**
+     * Get info for a transaction id
+     * @param  string $args 
+     * @return string
+     */
+    
     public function txInfo($args) {
         $data = $this->request('/transactions/info/' . $args);
         return $data;
     }
+    
+    /**
+     * Get latest transactions for an address
+     * @param  array $args 
+     * @return string
+     */
     
     public function txLatest($args) {
         $data = $this->request('/transactions/address/' . $args['address'] . '/' . $args['limit']);
         return $data;
     }
     
+    /**
+     * Get block at specified heights
+     * @param  array $args 
+     * @return string
+     */
+    
     public function getBlocks($args) {
         $data = $this->request('/blocks/seq/' . $args['start'] . '/' . $args['end']);
         return $data;
     }
     
-    public function getNodes() {
+    /**
+     * Get genesis block data
+     * @return string
+     */
+    
+    public function getBlocksFirst() {
+        $data = $this->request('/blocks/first/');
+        return $data;
+    }
+    
+    /**
+     * Get last block data
+     * @return string
+     */
+    
+    public function getBlocksLast() {
+        $data = $this->request('/blocks/last/');
+        return $data;
+    }
+    
+    /**
+     * Full peer list
+     * @return string
+     */
+    
+    public function getPeers() {
+        $data = $this->request('/peers/all');
+        return $data;
+    }
+    
+    /**
+     * Connected peers list
+     * @return string
+     */
+    
+    public function getPeersConnected() {
         $data = $this->request('/peers/connected');
+        return $data;
+    }
+    
+    /**
+     * Blacklisted peers list
+     * @return string
+     */
+    
+    public function getPeersBlacklisted() {
+        $data = $this->request('/peers/blacklisted');
+        return $data;
+    }
+    
+    /**
+     * Suspended peers list
+     * @return string
+     */
+    
+    public function getPeersSuspended() {
+        $data = $this->request('/peers/suspended');
         return $data;
     }
 }
